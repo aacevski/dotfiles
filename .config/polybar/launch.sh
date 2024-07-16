469e8bd4e6c921038b32f8d365e8ea3f419f5c51
Andrej Acevski <aacevski@gmail.com>
2024-07-16 18:54:32 +0200

    Initial commit

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+
+# Launch example bar
+polybar -config $HOME/.config/polybar/config.ini main 2>&1 | tee -a /tmp/polybar-main.log & disown
+polybar -config $HOME/.config/polybar/config.ini external 2>&1 | tee -a /tmp/polybar-external.log & disown
---
+++ .zshrc
@@ -0,0 +1,19 @@
+# oh-my-zsh Setup
+export ZSH="$HOME/.oh-my-zsh"
+source $ZSH/oh-my-zsh.sh
+
+# Theme
+ZSH_THEME="robbyrussell"
+
+# Plugins
+plugins=(git zsh-autosuggestions)
+
+# Scripts
+export PATH="/home/andrej/.config/scripts:$PATH"
+
+# Starship 
+eval "$(starship init zsh)"
+
+# Zoxide
+export PATH="/home/andrej/.local/bin:$PATH"
+eval "$(zoxide init zsh)"
