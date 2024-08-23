ad0701ecad8ee01b3acd438321b767ec7b050453
Andrej Acevski <aacevski@gmail.com>
2024-08-23 21:17:13 +0200

    Improving color scheme + adding nvim-surround

--- .config/nvim/lua/plugins/gruvbox.lua
+++ .config/nvim/lua/plugins/gruvbox.lua
@@ -1,7 +1,19 @@
 return {
-	"f4z3r/gruvbox-material.nvim",
-	name = "gruvbox-material",
-	lazy = false,
-	priority = 1000,
-	opts = {},
+  {
+    "f4z3r/gruvbox-material.nvim",
+    name = "gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "mawkler/modicator.nvim",
+    dependencies = "f4z3r/gruvbox-material.nvim",
+    init = function()
+      vim.o.cursorline = true
+      vim.o.number = true
+      vim.o.termguicolors = true
+    end,
+    opts = {}
+  }
 }
