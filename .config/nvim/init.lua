5abd120468da06f6aa8df3561f27161ebc22ec2c
Andrej Acevski <aacevski@gmail.com>
2024-09-08 22:29:25 +0200

    rehaul of nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,27 @@
-require("config.options")
-require("config.lazy")
+require 'core.options'
+require 'core.keymaps'
+
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup {
+  require 'plugins.colors',
+  require 'plugins.neotree',
+  require 'plugins.bufferline',
+  require 'plugins.lualine',
+  require 'plugins.treesiter',
+  require 'plugins.telescope',
+  require 'plugins.lsp',
+  require 'plugins.autocompletion',
+  require 'plugins.nonels',
+  require 'plugins.gitsigns',
+  require 'lua.plugins.indentblankline',
+  require 'lua.plugins.misc',
+}
